85646c60250c3467a51cc10585ff58fa6183a652
Chescore <stevechesa@gmail.com>
2023-01-30 20:33:59 +0300

    New Commit

--- client/src/api/index.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const URL = 'https://haicore.herokuapp.com/';
-
-export const fetchPosts = () => axios.get(URL);
-export const fetchUsersPosts = () => axios.get(`${URL}/userPosts`);
-export const createPosts = (newPost) => axios.post(URL, newPost);
-export const updatePosts = (id,updatedPost) => axios.patch(`${URL}/userPosts/${id}`,updatedPost);
-export const deletePosts = (id) => axios.delete(`${URL}/userPosts/${id}`)
--- client/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import {combineReducers} from 'redux';
-
-import {postReducers,userPostReducers} from './posts';
-
-export default combineReducers({
-    postReducers,
-    userPostReducers
-})
